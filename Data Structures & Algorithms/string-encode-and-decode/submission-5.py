be5779cfbbe4f643e2ee9e6249ae478d5aa96252
romanzrespicio <roman.respicio05@gmail.com>
2026-06-23 21:37:50 -0700

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        out = ''
+
+        for s in strs:
+            out += str(len(s)) + '/' + s
+
+        return out
+
+    def decode(self, s: str) -> List[str]:
+
+        out = []
+        i = 0
+        j = i
+
+        for i in range(len(s)):
+            i = j
+            if i == len(s):
+                break
+            while s[i] != '/':
+                i += 1     
+            length = int(s[j:i])
+            i = i + 1
+            j = i + length
+            out.append(s[i:j])
+
+        return out
+
